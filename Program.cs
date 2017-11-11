4bf84a99e6dd45067d62a2fa0964de49b881a890
Phong <xuanphongit1109@gmail.com>
2017-11-11 11:27:54 +0700

    Create

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace T02_Source_Code
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
